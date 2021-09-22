af63379f584a023c996f20fd17e58b31b46c1477
Tom Saeger <tom.saeger@gmail.com>
2021-09-22 15:22:18 -0500

    cyrus-sasl-xoauth2: 0.2

---
+++ pkgs/development/libraries/cyrus-sasl/plugins/xoauth2/default.nix
@@ -0,0 +1,33 @@
+{stdenv, fetchFromGitHub, pkgconfig, autoconf, automake, libtool
+, cyrus-sasl
+}:
+
+stdenv.mkDerivation rec {
+  pname = "cyrus-sasl-xoauth2";
+  version = "0.2";
+
+  src = fetchFromGitHub {
+    owner = "moriyoshi";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1py9f1mn5k5xihrk0lfrwr6723c22gjb7lmgya83ibvislm2x3wl";
+  };
+
+  nativeBuildInputs = [ autoconf automake libtool pkgconfig ];
+  buildInputs = [ cyrus-sasl.dev ];
+
+  preConfigure = "./autogen.sh";
+
+  installPhase = ''
+    mkdir -p $out
+    make install DESTDIR=$out
+    '';
+
+  meta = with stdenv.lib; {
+    description = "cyrus-sasl plugin implementation of XOAUTH2";
+    homepage = "https://github.com/moriyoshi/cyrus-sasl-xoauth2";
+    license = licenses.mit;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ ];
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12155,6 +12155,18 @@ in
     kerberos = if stdenv.isFreeBSD then libheimdal else kerberos;
   };
 
+  cyrus-sasl-xoauth2 = callPackage ../development/libraries/cyrus-sasl/plugins/xoauth2 {
+    cyrus-sasl = cyrus_sasl;
+  };
+
+  cyrus-sasl-with-plugins = cyrus_sasl.overrideAttrs (oa: rec {
+    postInstall = ''
+      for lib in ${cyrus-sasl-xoauth2}/usr/lib/sasl2/*; do
+      ln -sf $lib $out/lib/sasl2/
+      done
+    '';
+  });
+
   # Make bdb5 the default as it is the last release under the custom
   # bsd-like license
   db = db5;
