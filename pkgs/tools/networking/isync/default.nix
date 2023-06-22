00f605ba57966aae3dac8a1abcb8b9133c16f378
Tom Saeger <tom.saeger@gmail.com>
2023-06-22 11:31:15 -0500

    isync: enable cyrus-sasl-xoauth2 by default

--- pkgs/tools/networking/isync/default.nix
+++ pkgs/tools/networking/isync/default.nix
@@ -3,7 +3,7 @@
 , Security
 # Disabled by default as XOAUTH2 is an "OBSOLETE" SASL mechanism and this relies
 # on a package that isn't really maintained anymore:
-, withCyrusSaslXoauth2 ? false, cyrus-sasl-xoauth2, makeWrapper
+, withCyrusSaslXoauth2 ? true, cyrus-sasl-xoauth2, makeWrapper
 }:
 
 stdenv.mkDerivation rec {
